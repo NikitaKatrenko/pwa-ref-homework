942cdf8e4ff6858d12e11387c85670c5ef23ebd1
Nikita <thanqdj@gmail.com>
2024-07-28 17:14:48 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyCGA5-g6ibCAgv52kOrIjVPRGN3OfgCXr0",
-    authDomain: "pwa-reference.firebaseapp.com",
-    projectId: "pwa-reference",
-    storageBucket: "pwa-reference.appspot.com",
-    messagingSenderId: "346562453988",
-    appId: "1:346562453988:web:92db23fd9d3b955bb267f1",
-    databaseURL: "https://pwa-reference.firebaseio.com"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    databaseURL: ""
 };
 
 const app = initializeApp(firebaseConfig);
